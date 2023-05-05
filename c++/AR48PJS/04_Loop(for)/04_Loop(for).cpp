5d80cc2bb248156e0d81bca8a8d9e701ae5ee503
aewtwa <1374000@naver.com>
2023-05-05 12:32:59 +0900

    박정수 : 숙제 1일차

--- c++/AR48PJS/04_Loop(for)/04_Loop(for).cpp
+++ c++/AR48PJS/04_Loop(for)/04_Loop(for).cpp
@@ -5,16 +5,63 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int test = 0;
+    test = 123;
+
+    // 반복문 (코드를 반복해서 실행한다.)
+
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+    //std::cout << "Hello World!\n";
+
+    //int i;
+    //for (i = 0; i < 10; i++)
+    //{
+    //    std::cout << "Hello World!\n";
+    //}
+    //int i;
+    //for (초기화; 조건식; 증감식)
+    //{
+    //
+    //}
+    // 초기식 , 조건식, 증감식은 프로그래머
+    // 마음대로 바꿀수 있다.
+
+    int d = 0;
+    std::cin >> d;
+    int k;
+    for (k = 0; k < d; k++)
+    {
+        std::cout << "Hello World!\n";
+    }
+
+    //int k;
+    //for (k = 1; k < 2; k++)
+    //{
+    //    std::cout << "Hello World!\n";
+    //}
+
+    //for문 안에도 if문을 사용이 가능하다.
+    int j;
+    for (j = 0; j < 10; j++)
+    {
+        if (j % 2 == 0)
+        {
+            std::cout << j << " 는 짝수입니다.\n";
+        }
+        else
+        {
+            std::cout << j << " 는 홀수입니다.\n";
+        }
+    }
+
+    return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
