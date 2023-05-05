92ce966cb6b0b8d345fbfe7dc09a262cd63df7d8
aewtwa <1374000@naver.com>
2023-05-05 12:46:01 +0900

    박정수 : 숙제 1.5

---
+++ HomeWork/HomeWork/HomeWorkPractice/HomeWorkPractice.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int q = 1;
+	int w = 2;
+	int e = 3;
+	std::cout << "#q=" << q << ", " << q << ", " << q << endl;
+	std::cout << "#w=" << w << ", " << w << ", " << w << endl;
+	std::cout << "#e=" << e << ", " << e << ", " << e << endl;
+}
