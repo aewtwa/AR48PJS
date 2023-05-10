a19ef33c0a6b6f982a115b90d89d4b2139f69e97
aewtwa <1374000@naver.com>
2023-05-10 19:01:13 +0900

    박정수 : 숙제노트 이름변경

--- c++/AR48PJS/11_Struct/11_Struct.cpp
+++ c++/AR48PJS/11_Struct/11_Struct.cpp
@@ -1,20 +1,68 @@
-﻿// 11_Struct.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
 
-#include <iostream>
+
+// 사용자 정의 자료형
+
+
+// 구조체는 접근제한 지정가가 기본값이 public이다
+
+struct Player
+{
+    int hp;
+    int mp;
+    int att;
+    int def;
+};
+
+//struct Player
+//{
+//public:
+//    int hp;
+//    int mp;
+//
+//private:
+//    int att;
+//    int def;
+//};
+
+// 클래스에는 접근 제한 지정자가 있다
+// 클래스는 디폴트(기본) 접근제한 지정자가 private;
+//class Player
+//{
+//public:
+//    int hp;
+//    int mp;
+//
+//private:
+//    int att;
+//    int def;
+//};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //int playerHP = 0;
+    //int playerMP;
+    //int playerATT;
+    //int playerDEF;
+
+    //int player2HP;
+    //int player2MP;
+    //int player2ATT;
+    //int player2DEF;
+
+    Player warrior;
+    warrior.mp = 100;
+    warrior.hp = 100;
+    //warrior.
+
+    Player archer;
+    Player magicion;
+
+    // 구조체를 쓰는 이유
+    // 여러개의 변수들을 묶은 형태로 그룹화 하고 싶을때
+    // 특성 상황에서 소스코드를 보다 간결하게 표현할수 있다.
+    // 차후에 자료구조 및 알고리즘을 배울 때 구조체,클래스가 사용되는데
+    // 이를 위해 미리 익숙해져야 한다.
+
+    return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
