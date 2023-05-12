2c3677eb67fc6ee77557832cbdb8bc13955714f2
aewtwa <1374000@naver.com>
2023-05-12 22:38:29 +0900

    박정수 : 숙제 Level6.5

--- HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
+++ HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
@@ -1,31 +1,10 @@
 ﻿#include <iostream>
 
-struct Person
-{
-	int age;
-	int height;
-};
-
-Person a = {};
-Person b = {};
-
-void input()
-{
-	std::cin >> a.age;
-	std::cin >> a.height;
-	std::cin >> b.age;
-	std::cin >> b.height;
-}
-
-void output()
-{
-	std::cout << (a.age + b.age) / 2;
-	std::cout << " ";
-	std::cout << (a.height + b.height) / 2;
-}
-
 int main()
 {
-	input();
-	output();
+	char a;
+	std::cin >> a;
+	char b;
+	std::cin >> b;
+	std::cout << b - a;
 }
