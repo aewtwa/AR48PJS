b7a05736cebcfaea8bdfe1aa35ed30f87eddaa5f
aewtwa <1374000@naver.com>
2023-05-15 01:45:01 +0900

    박정수 : 콘솔게임만들기연습(class구조만들기)

--- Practice/Practice/Practice.cpp
+++ Practice/Practice/Practice.cpp
@@ -1,12 +1,77 @@
 ﻿#include <iostream>
 #include <conio.h>
 #include <Windows.h>
+#include <vector>
+
+class Map
+{
+public:
+
+static const int MapXsize = 5;
+static const int MapYsize = 5;
+
+protected:
+
+private:
+	Map()
+	{
+	}
+};
+
+class int2
+{
+public:
+	int2(int _X,int _Y)
+		:mX(_X)
+		,mY(_Y)
+	{
+
+	}
+	int mX;
+	int mY;
+};
+
+class GameObject
+{
+public:
+	GameObject()
+		:mObjectPrinter(0)
+		, mPos(int2(0,0))
+	{
+
+	}
+	// setter
+	void SetObjectPrinter(char _Printer)
+	{
+		mObjectPrinter = _Printer;
+	}
+
+	void SetPos(int2 _Pos)
+	{
+		mPos = _Pos;
+	}
+	int2 GetPos()
+	{
+		return mPos;
+	}
+protected:
+
+private:
+	char mObjectPrinter;
+	int2 mPos;
+};
+
+class Player
+{
+};
+
+std::vector< GameObject> GameObjects = {};
+GameObject GamePlayer;
 
 const int MapXsize = 5;
 const int MapYsize = 5;
-int PlayerXPos = 2;
-int PlayerYPos = 2;
 int arr2d[MapYsize][MapXsize] = {};
+
 void SetMap()
 {
 	char MapPrint = 'a';
@@ -19,51 +84,50 @@ void SetMap()
 	}
 }
 
-void SetPlayer()
-{
-	char PlayerPrint = '@';
-	arr2d[PlayerYPos][PlayerXPos] = PlayerPrint;
-}
 
 void PlayerMove()
 {
+	int2 PlayerPos = GamePlayer.GetPos();
+
 	if (0 == _kbhit())
 	{
 		Sleep(0);
 		return;
 	}
 	char a = _getch();
+	
 	switch (a)
 	{
 	case 'a':
 	case 'A':
-		if (PlayerXPos > 0)
+		if (PlayerPos.mX > 0)
 		{
-			PlayerXPos -= 1;
+			PlayerPos.mX -= 1;
 		}
 		break;
 	case 'd':
 	case 'D':
-		if (PlayerXPos < 4)
+		if (PlayerPos.mX < 4)
 		{
-			PlayerXPos += 1;
+			PlayerPos.mX += 1;
 		}
 		break;
 	case 'w':
 	case 'W':
-		if (PlayerYPos > 0)
+		if (PlayerPos.mY > 0)
 		{
-			PlayerYPos -= 1;
+			PlayerPos.mY -= 1;
 		}
 		break;
 	case 's':
 	case 'S':
-		if (PlayerYPos < 4)
+		if (PlayerPos.mY < 4)
 		{
-			PlayerYPos += 1;
+			PlayerPos.mY += 1;
 		}
 		break;
 	}
+	GamePlayer.SetPos(PlayerPos);
 }
 
 void MapPrint()
@@ -72,20 +136,29 @@ void MapPrint()
 	{
 		for (int j = 0; j < 5; j++)
 		{
-			std::cout << (char)(arr2d[i][j]);
+			std::cout << 'a';
 		}
 		std::cout << "\n";
 	}
+
+	/*
+		gameobjects들을 순회하면서 gameobject 들을 출력해보세요
+	*/
+
+
 }
 
 int main()
 
 {
+	GameObjects.push_back(GamePlayer);
+
+	GamePlayer.SetObjectPrinter('@');
+	GamePlayer.SetPos(int2(2, 2));
 	while (true)
 	{
 		system("cls");
 		SetMap();
-		SetPlayer();
 		PlayerMove();
 		MapPrint();
 		Sleep(100);
