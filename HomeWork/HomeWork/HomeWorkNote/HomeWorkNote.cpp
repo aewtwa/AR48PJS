7485e78435ded4829da2eedb344caa764cb7f89c
aewtwa <1374000@naver.com>
2023-05-17 16:01:50 +0900

    박정수 : 숙제 Level12

--- HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
+++ HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
@@ -2,10 +2,27 @@
 
 int main()
 {
-	char a;
+	char a[256];
 	std::cin >> a;
-	for (int i = 0; i < 5; i++)
+	char b[256];
+	std::cin >> b;
+	int check = 0;
+	for (int i = 0; i < 256; i++)
 	{
-		std::cout << a;
+		if (a[i] == '\0')
+		{
+			check = i;
+			break;
+		}
 	}
+	std::cout << check;
+	for (int i = 0; i < 256; i++)
+	{
+		if (b[i] == '\0')
+		{
+			check = i;
+			break;
+		}
+	}
+	std::cout << check;
 }
