82326d5729b530ecf165cd0e12276ff5d420926f
aewtwa <1374000@naver.com>
2023-05-23 09:52:02 +0900

    박정수 : 숙제Level18

--- HomeWork/HomeWork/HomeWork Level_18/HomeWork Level_18.cpp
+++ HomeWork/HomeWork/HomeWork Level_18/HomeWork Level_18.cpp
@@ -1,10 +1,28 @@
 ﻿//문제 1번
+//cardList 배열에 여러종류의 카드가 있습니다.
+//아래와 같이 카드를 입력받아주세요(한 문장을 입력받아주세요, 최대 15글자)
+//DAT(Direct Address Table)을 이용하여 총 몇 종류의 카드가 있는지 count 해서 출력 해주세요.
+//ABCDACABCDE
+//출력: 5개
+//입력 예제
+//ABCDACABCDE
+//출력 결과
+//5개
 
 #include <iostream>
 
 int main()
 {
-
+	char cardlist[16] = {};
+	std::cin >> cardlist;
+	int arr[256] = {};
+	for (size_t i = 0; i < 256; i++)
+	{
+		if (cardlist[i] == arr[i])
+		{
+			arr[i]++;
+		}
+	}
 }
 
 //문제 2번
--- HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
+++ HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
@@ -1,50 +1,12 @@
 ﻿#include <iostream>
 
-void isSame(char* _a, char* _b)
-{
-    int flag = 0;
-    int same = 0;
-    for (size_t i = 0; i < 256; i++)
-    {
-        for (size_t j = i; j < 256; j++)
-        {
-            if (_a[i] == '\0')
-            {
-                flag = 1;
-                break;
-            }
-            if (_a[i] == _b[j])
-            {
-                same = 1;
-                break;
-            }
-            else
-            {
-                same = 0;
-                flag = 1;
-                break;
-            }
-        }
-        if (flag == 1)
-        {
-            break;
-        }
-    }
-    if (same == 1)
-    {
-        std::cout << "동명";
-    }
-    else
-    {
-        std::cout << "남남";
-    }
-}
-
 int main()
 {
-    char a[256] = {};
-    std::cin >> a;
-    char b[256] = {};
-    std::cin >> b;
-    isSame(a, b);
+	char cardlist[16] = {};
+	std::cin >> cardlist;
+	int arr[256] = {};
+	for (size_t i = 0; i < 256; i++)
+	{
+		arr[i] = i;
+	}
 }
