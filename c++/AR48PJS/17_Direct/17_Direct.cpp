f9389295972cfcdabae0e00f2df7a35f16850151
aewtwa <1374000@naver.com>
2023-05-23 10:04:07 +0900

    박정수 : 5/22 수업

---
+++ c++/AR48PJS/17_Direct/17_Direct.cpp
@@ -0,0 +1,46 @@
+﻿// 17_Direct.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int map[5][4] = {};
+
+
+int main()
+{
+
+    int x = 1;
+    int y = 2;
+
+
+    // 하드코딩 방식
+    int left = map[y][x - 1];
+    int right = map[y][x + 1];
+    int top = map[y - 1][x];
+    int bottom = map[y + 1][x];
+
+
+    // Direct 방식
+    int offset[4][2] =
+    {
+        0, 1, //right
+        0, -1, //left
+        1, 0, //bottom
+        -1, 0, //top
+    };
+
+    for (size_t i = 0; i < 4; i++)
+    {
+        int newX = x + offset[i][1];
+        int newY = y + offset[i][0];
+
+        int data = map[newY][newX];
+
+        if (data)
+        {
+
+        }
+    }
+    return 0;
+}
+
---
+++ c++/AR48PJS/18_DirectAddressTable/18_DirectAddressTable.cpp
@@ -0,0 +1,50 @@
+﻿// 18_DirectAddressTable.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int hashFucntion(int data)
+{
+    int index = 0;
+
+    index = data * 7 / 32 + 22 - 108 * 32;
+    return index;
+
+    // 중복값이 나올수있다.
+    // 지구상에서 해결 불가능 아직은
+    // 어떤 해시 함수도 천만분의 1억만분의 1
+}
+
+
+int main()
+{
+    char str[256] = "ASDVVSDFEBDSSFAJEJFKDKEJZ";
+
+    char target = 'A'; // 65
+    // A가 나올떄마다 A의 갯수를 세주는데
+
+    int bucket[128] = {};
+
+
+    for (size_t i = 0; i < 256; i++)
+    {
+        char target = str[i];
+        bucket[target]++;
+    }
+    int len = bucket['B'];
+
+
+    // 정렬도 편리하다'
+    for (size_t i = 1; i < 128; i++)
+    {
+        if (bucket[i] > 0)
+        {
+            for (size_t j = 0; j < bucket[i]; j++)
+            {
+                std::cout << (char)i;
+            }
+        }
+    }
+
+    return 0;
+}
