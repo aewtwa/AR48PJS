1c66e89cef9d4cd27a442ebd10a8ffd7a93cddba
aewtwa <1374000@naver.com>
2023-06-01 22:35:59 +0900

    박정수 : 숙제Level25

--- HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
+++ HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
@@ -1,26 +1,35 @@
 ﻿#include <iostream>
 
-int number[6] = {};
+char str[11] = {};
+
+int isPair()
+{
+	int bucket[256] = {};
+	for (int i = 0; i < 10; i++)
+	{
+		int target = str[i];
+		bucket[target]++;
+		if (bucket[70] > bucket[66])
+		{
+			return 1;
+		}
+	}
+	if (bucket[66] != bucket[70])
+	{
+		return 1;
+	}
+	return 0;
+}
 
 int main()
 {
-	for (size_t i = 0; i < 6; i++)
+	std::cin >> str;
+	if (isPair() == 1)
 	{
-		std::cin >> number[i];
+		std::cout << "짝이 안 맞음";
 	}
-
-	char command[7] = {};
-	std::cin >> command;
-
-	for (int i = 0; i < 6; i++)
+	else
 	{
-		if (command[i] == 'm')
-		{
-
-		}
-		else
-		{
-
-		}
+		std::cout << "짝이 맞음";
 	}
 }
