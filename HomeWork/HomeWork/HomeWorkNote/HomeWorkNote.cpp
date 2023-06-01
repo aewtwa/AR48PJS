14f1ed738638c02cc1d76c518d04b36f244f286a
aewtwa <1374000@naver.com>
2023-06-01 11:31:51 +0900

    박정수 : 숙제 Level25

--- HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
+++ HomeWork/HomeWork/HomeWorkNote/HomeWorkNote.cpp
@@ -1,25 +1,26 @@
 ﻿#include <iostream>
 
-char dicenum[6] = { '1','2','3','4','5','6' };
-char path[6] = {};
-void dice(int _level, int _a)
-{
-	if (_level == _a)
-	{
-		std::cout << path << "\n";
-		return;
-	}
-	for (size_t i = 0; i < 6; i++)
-	{
-		path[_level] = dicenum[i];
-		dice(_level + 1, _a);
-		path[_level] = 0;
-	}
-}
+int number[6] = {};
 
 int main()
 {
-	int n = 0;
-	std::cin >> n;
-	dice(0, n);
+	for (size_t i = 0; i < 6; i++)
+	{
+		std::cin >> number[i];
+	}
+
+	char command[7] = {};
+	std::cin >> command;
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (command[i] == 'm')
+		{
+
+		}
+		else
+		{
+
+		}
+	}
 }
