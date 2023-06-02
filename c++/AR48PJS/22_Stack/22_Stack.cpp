58c8d5e673049cc0ff99a348ba65e6fa109b4111
aewtwa <1374000@naver.com>
2023-06-02 20:21:51 +0900

    박정수 : 6/2 수업

--- c++/AR48PJS/22_Stack/22_Stack.cpp
+++ c++/AR48PJS/22_Stack/22_Stack.cpp
@@ -6,11 +6,83 @@
 
 // first input -> last out
 
+// 리스트 방식, 배열을 써서 구현하는 방식 2가지
+
+
+class Stack
+{
+public:
+    struct Node // 이너클래스
+    {
+        Node()
+            : data(0)
+            , next(nullptr)
+            , prev(nullptr)
+        {
+
+        }
+
+        int data;
+        Node* prev;
+        Node* next;
+    };
+    Stack()
+        : mHead(nullptr)
+        , mTail(nullptr)
+    {
+
+    }
+
+    void Push(int number)
+    {
+        if (mHead == nullptr)
+        {
+            mHead = new Node();
+            mHead->data = number;
+            mHead->prev = nullptr;
+            mHead->next = nullptr;
+            mTail = mHead;
+        }
+        else
+        {
+            mTail->next = new Node();
+            mTail->next->data = number;
+
+            mTail->next->prev = mTail;
+            mTail->next->next = nullptr;
+            mTail = mTail->next;
+        }
+    }
+    void Pop()
+    {
+        Node* last = mTail;
+
+        mTail = mTail->prev;
+        mTail->next = nullptr;
+
+        delete last;
+        last = nullptr;
+    }
+    int Top()
+    {
+        return mTail->data;
+    }
+
+private:
+    Node* mHead;
+    Node* mTail;
+};
+
 int main()
 {
+    Stack stack;
 
+    stack.Push(1);
+    stack.Push(2);
+    stack.Push(3);
 
-
+    int top = stack.Top();
+    stack.Pop();
 
     return 0;
 }
--- c++/AR48PJS/23_Queue/23_Queue.cpp
+++ c++/AR48PJS/23_Queue/23_Queue.cpp
@@ -3,11 +3,54 @@
 // 큐
 // first in -> first out
 
+// 템플릿 클래스
+// 내가 어떤자료형이든지 간에
+// 템플릿을 이용해서 만들어놓으면
+// 사용할떄 사용할 자료형을 명시해주면
+// 해당자료형으로 클래스가 구성이된다.
+
+template <typename T>
+class Queue
+{
+public:
+    Queue()
+        : mDatas{}
+        , mFront(0)
+        , mEnd(0)
+    {
+
+    }
+
+    void Push(T data)
+    {
+        mDatas[mEnd] = data;
+        mEnd++;
+    }
+
+    T Pop()
+    {
+        return mDatas[mFront++];
+    }
+
+private:
+    T mDatas[256];
+    int mFront;
+    int mEnd;
+};
 
 int main()
 {
+    Queue<int> queue;
+    queue.Push(1);
+    queue.Push(2);
+    queue.Push(3);
+    queue.Push(4);
 
+    int pop = queue.Pop();
+
+    //Queue<char> queue;
 
 
     return 0;
 }
+
