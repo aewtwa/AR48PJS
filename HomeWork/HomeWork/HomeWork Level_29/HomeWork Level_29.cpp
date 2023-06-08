d9be459a6b83f6af3e4f1b12fb382897065522cd
aewtwa <1374000@naver.com>
2023-06-08 23:59:15 +0900

    박정수 : 숙제

--- HomeWork/HomeWork/HomeWork Level_29/HomeWork Level_29.cpp
+++ HomeWork/HomeWork/HomeWork Level_29/HomeWork Level_29.cpp
@@ -526,9 +526,50 @@ int main()
 
 #include <iostream>
 
+int arr[3][3] = {};
+
+void isSame(int _a)
+{
+	int flag = 0;
+	int a = 0;
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = i + 1; j < 3; j++)
+		{
+			if (arr[_a][i] != arr[_a][j])
+			{
+				flag = 1;
+			}
+			else
+			{
+				a = arr[_a][i];
+			}
+		}
+	}
+	if (flag == 1)
+	{
+		std::cout << "X" << "\n";
+	}
+	else
+	{
+		std::cout << a << "\n";
+	}
+}
+
 int main()
 {
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++)
+		{
+			std::cin >> arr[i][j];
+		}
+	}
 
+	for (size_t i = 0; i < 3; i++)
+	{
+		isSame(i);
+	}
 }
 
 //문제 13번
